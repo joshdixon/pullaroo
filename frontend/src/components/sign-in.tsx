0b07a07048be8536a90ba31fd949e1a95e70fc1c
joshdixon <6028199+joshdixon@users.noreply.github.com>
2023-09-05 22:20:41 +0930

    sign in button

---
+++ frontend/src/components/sign-in.tsx
@@ -0,0 +1,23 @@
+﻿"use client";
+
+import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
+
+export function SignIn() {
+    const router = useRouter();
+    
+    const handleLoginClick = () => {
+        const clientId = "Iv1.5b7afd4137446a44";
+
+        // const codeVerifier = generateCodeVerifier();
+        // const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+        const authUrl = `https://github.com/login/oauth/authorize?&client_id=${clientId}`
+
+        router.push(authUrl);
+    };
+    
+    return (
+        <Button onClick={handleLoginClick}>Login with GitHub</Button>
+    );
+};
